85cdb4810a75854b60046dd4c0c7ab7677c89427
TJ Nicolaides <tj.nicolaides@thinkbrownstone.com>
2018-10-23 16:04:38 -0400

    THINKDOCS-58 #comment messy example using the unaltered Hub SCSS

--- site/src/templates/standardsTemplate.js
+++ site/src/templates/standardsTemplate.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
+import "../scss/main.scss"
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
@@ -7,8 +8,8 @@ export default function Template({
   const { markdownRemark } = data // data.markdownRemark holds our post data
   const { frontmatter, html } = markdownRemark
   return (
-    <div className="blog-post-container">
-      <div className="blog-post">
+    <section className="hub-section">
+      <div className="hub-section-heading">
         <h1>{frontmatter.title}</h1>
         <h2>{frontmatter.date}</h2>
         <div
@@ -16,7 +17,7 @@ export default function Template({
           dangerouslySetInnerHTML={{ __html: html }}
         />
       </div>
-    </div>
+    </section>
   )
 }
 
