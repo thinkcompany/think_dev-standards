c0de2a8193fee0ef4c771ed5383ad754e4cd511d
Nicole Witte <nicole.witte@thinkbrownstone.com>
2018-11-01 16:28:20 -0400

    add padding to top of layout, remove classnames on list items

--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -6,34 +6,43 @@ import '../scss/main.scss';
 
 const IndexPage = () => (
   <Layout>
+    <h2>Think Company Front-End Web Development Standards</h2>
+    <p>The basic principles of the Think Company Front-End Web Development Standards are simple: keep code modular and flexible; support the greatest number of browsers without overwhelming older browsers with polyfills and hacks; ensure graceful degradation and accessibility at all times; and keep the experience light and fast. Developers should speak up any time they feel that they cannot meet the standards, or when they see an opportunity to add a common practice to these standards. </p>
+    <p>The Front-End Web Development Standards are broken down into coding guidelines for HTML, CSS, JavaScript, Accessibility, SEO, and Performance: some information is duplicated across sections for easy reference. </p>
+    <h3>Chapters</h3>
     <ul>
       <li><Link to="/html/">HTML Standards</Link></li>
       <li>Style Sheets</li>
       <ul>
-        <li><Link className="hub-entry-link" to="/css/">CSS Standards</Link></li>
-        <li><Link className="hub-entry-link" to="/sass/">Sass Standards</Link></li>
+        <li><Link to="/css/">CSS Standards</Link></li>
+        <li><Link to="/sass/">Sass Standards</Link></li>
       </ul>
       <li>
-        <Link className="hub-entry-link" to="/js-standards/">JavaScript Standards</Link>
+        <Link to="/js-standards/">JavaScript Standards</Link>
       </li>
       <ul>
         <li>
-          <Link className="hub-entry-link" to="/general/">General JS Authoring</Link>
+          <Link to="/general/">General JS Authoring</Link>
         </li>
         <li>
-          <Link className="hub-entry-link" to="/jquery/">JQuery Authoring</Link>
+          <Link to="/jquery/">JQuery Best Practices</Link>
+        </li>
+        <li>
+          <Link to="/largeapps/">Structuring Large Client-side Applications</Link>
+        </li>
+        <li>
+          <Link to="/resources/">Resources</Link>
         </li>
       </ul>
-      <li><Link className="hub-entry-link" to="/accessibility">Accessibility Standards</Link></li>
+      <li><Link to="/accessibility">Accessibility Standards</Link></li>
       <li>Quality Assurance</li>
       <ul>
-        <li><Link className="hub-entry-link" to="/automated-testing/">Automated Testing Standards</Link></li>
-        <li><Link className="hub-entry-link" to="/quality-assurance/qa/">Quality Assurance</Link></li>
+        <li><Link to="/automated-testing/">Automated Testing Standards</Link></li>
+        <li><Link to="/quality-assurance/qa/">Quality Assurance</Link></li>
       </ul>
-      <li><Link className="hub-entry-link" to="/git/">Git Standards</Link></li>
-      <li><Link className="hub-entry-link" to="/performance/">Performance Standards</Link></li>
-      <li><Link className="hub-entry-link" to="/seo/">SEO Standards</Link></li>
-      <li><Link className="hub-entry-link" to="/resources/">Resources</Link></li>
+      <li><Link to="/git/">Git Standards</Link></li>
+      <li><Link to="/performance/">Performance Standards</Link></li>
+      <li><Link to="/seo/">SEO Standards</Link></li>
     </ul>
   </Layout>
 )
