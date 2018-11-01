8e39552e826176d9067b71224f2af5b5a8fbcd07
Nicole Witte <nicole.witte@thinkbrownstone.com>
2018-11-01 15:51:39 -0400

    update header structure

--- site/src/components/header.js
+++ site/src/components/header.js
@@ -1,31 +1,26 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import '../scss/main.scss';
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <div className='hub-util-header'>
+    <div className='hub-util-header-inner'>
+      <div class="svg-logo-think think-logo-top">
+        <span class="visually-hidden">Think Company</span>
+      </div>
+      <div>
+        <h1 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: 'white',
+              textDecoration: 'none',
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </h1>
+      </div>
     </div>
   </div>
 )
