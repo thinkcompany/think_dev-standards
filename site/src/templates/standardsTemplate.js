6322cb19b3ecc445d76a7dc5ce38fc6f3f5749ad
Nicole Witte <nicole.witte@thinkbrownstone.com>
2018-11-05 16:51:40 -0500

    add wrapping div and nav to standards template

--- site/src/templates/standardsTemplate.js
+++ site/src/templates/standardsTemplate.js
@@ -2,20 +2,25 @@ import React from "react"
 import { graphql } from "gatsby"
 import "../scss/main.scss"
 
+import Nav from "../components/nav"
+
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
 }) {
   const { markdownRemark } = data // data.markdownRemark holds our post data
   const { frontmatter, html } = markdownRemark
   return (
-    <section className="hub-section">
-      <div className="hub-section-heading">
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
-      </div>
-    </section>
+    <div>
+      <Nav />
+      <section className="hub-section">
+        <div className="hub-section-heading">
+          <div
+            className="blog-post-content"
+            dangerouslySetInnerHTML={{ __html: html }}
+          />
+        </div>
+      </section>
+    </div>
   )
 }
 
