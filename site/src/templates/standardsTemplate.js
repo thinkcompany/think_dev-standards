905883c69867dd51b4ea782d51bd162fdbd23b54
Nicole Witte <nicole.witte@thinkbrownstone.com>
2018-11-08 12:49:48 -0500

    remove unused variable

--- site/src/templates/standardsTemplate.js
+++ site/src/templates/standardsTemplate.js
@@ -11,7 +11,7 @@ export default function Template({
   data, // this prop will be injected by the GraphQL query below.
 }) {
   const { markdownRemark } = data // data.markdownRemark holds our post data
-  const { frontmatter, html } = markdownRemark
+  const { html } = markdownRemark
   return (
     <div>
       <Header />
