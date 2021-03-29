99fc52339aa824d2d97271acfec950d560ca82a9
John Young <john.young@thinkcompany.com>
2021-03-29 15:42:16 -0400

    Remove Rackspace deploy scripts

--- bin/deploy-staging.sh
+++
@@ -1,1 +0,0 @@
-rsync --rsync-path="sudo -u www-data rsync" -az -vv --no-p --no-g --chmod=ugo=rwX -O -e 'ssh -p 22' ./site/public/ ${DEPLOY_USER}@${DEPLOY_HOST}:/var/www/html/standards-staging/sites/dev/public_html
